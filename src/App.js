2844f8c06f01b14d5c26355fffdf94c8c0471b58
khouloud227 <khouloud.hmed@ensi-uma.tn>
2021-01-11 22:34:10 +0100

    hello

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Member from './components/Member';
+function App() {
+  return ( <div style={{backgroundColor:'black',height:'800px',width:'100%'}}>
+    <Member/>
+    </div>
+  );
+}
+
+export default App;
